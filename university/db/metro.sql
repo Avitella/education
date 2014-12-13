658d4e5cd28ddbc4689484ba1c50d2a523a2241b
avitella <avitella@yandex-team.ru>
2014-12-14 01:43:28 +0300

    added db data

---
+++ university/db/metro.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS `metro`;
+CREATE DATABASE IF NOT EXISTS `metro` DEFAULT CHARACTER SET utf8;
+
+USE `metro`;
+
+DROP TABLE IF EXISTS `MetroLine`;
+CREATE TABLE `MetroLine` (
+  `MetroLineID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(64) NOT NULL DEFAULT '',
+  PRIMARY KEY (`MetroLineID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `MetroStation`;
+CREATE TABLE `MetroStation` (
+  `MetroStationID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `MetroLineID` bigint(20) NOT NULL,
+  `Name` varchar(64) NOT NULL DEFAULT '',
+  PRIMARY KEY (`MetroStationID`),
+  KEY `MetroLineID` (`MetroLineID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `TransferHub`;
+CREATE TABLE `TransferHub` (
+  `TransferHubID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `StartsMetroStationID` bigint(20) NOT NULL,
+  `EndsMetroStationID` bigint(20) NOT NULL,
+  PRIMARY KEY (`TransferHubID`),
+  KEY `StartsMetroStationID` (`StartsMetroStationID`),
+  KEY `EndsMetroStationID` (`EndsMetroStationID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `TurnstileRecord`;
+CREATE TABLE `TurnstileRecord` (
+  `TurnstileRecordID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `MetroStationID` bigint(20) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`TurnstileRecordID`),
+  KEY `MetroStationID` (`MetroStationID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
---
+++ university/db/parse.py
@@ -0,0 +1,32 @@
+import json
+
+jsFile = open("metro.json")
+js = jsFile.readline()
+
+data = json.loads(js)
+
+lines = open("lines.tsv", "w")
+lines.write("LineID\tName\n")
+
+stations = open("stations.tsv", "w")
+stations.write("StationID\tLineID\tName\n")
+
+transfers = open("transfers.tsv", "w")
+transfers.write("StartsStationID\tEndsStationID\n")
+
+for l in data["lines"]:
+    lineId = l
+    name = data["lines"][l]["name"]
+    lines.write("%s\t%s\n" % (lineId, name))
+
+for s in data["stations"]:
+    stationId = s
+    name = data["stations"][s]["name"]
+    lineId = data["stations"][s]["lineId"]
+    stations.write("%s\t%s\t%s\n" % (stationId, lineId, name))
+
+for t in data["transfers"]:
+    ids = list(data["transfers"][t]["stationIds"])
+    for i in range(len(ids)):
+        for j in range(len(ids)):
+            transfers.write("%d\t%d\n" % (ids[i], ids[j]))
